90d23a35a945761fe41c16f191a4cff222146a69
Alexis Aguilar <alexmerona@gmail.com>
2022-08-03 19:52:28 -0400

    08032022

---
+++ template.js
@@ -0,0 +1,12 @@
+//URL--
+// 
+
+//INSTRUCTIONS--
+/*
+
+*/
+
+//SOLUTION--
+
+
+//TESTCASES--
--- two-sum.js
+++ two-sum.js
@@ -25,30 +25,36 @@ Constraints:
 */
 
 //SOLUTION--
-/* 
-Iterate through the array
-I am going to check if the next element is equal to the target - the target number
-    If not, check to see if the number is in the hashmap
-        If not, add it with the property of the element's value and a value of its index
-        If so, return the index of the number in the hash map and the current element's index in an array
-*/
 
+// Without Hashmap:
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- */
- function twoSum(nums, target) {
-    const hashMap = {}
-    for (let i = 0; i < nums.length; i++) {
-        const num1 = nums[i]
-        const num2 = target - num1
-        if (hashMap[num2] !== undefined) {
-            return [hashMap[num2], i]
-        }
-        hashMap[num1] = i
+*/
+
+// I am given an array of integers "num" and I want two integers to add up to be our "target" number
+// So if I have an array nums = [2,7,11,15] and my target is 9, then I can clearly see that 2 + 7 = 9.
+// I am told to return the indeces of those numbers, which would be 0 and 1, respectfully.
+// So essentially, I am looking through the array for two numbers that will add up to the sum that is "target"
+// I immediately know that I will need a for loop to iterate through the array to grab a number.
+// I know I will need to grab a second number, essentially simultaneously, so that I can add that second number to the first number.
+// Therefore, I will need a second for loop.
+// Then, after grabbing one number with a for loop, grabbing a second number with the second for loop, I will need to compare these numbers
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) { // iterate through the array of numbers to grab our first integer
+        for (let j = i+1; j < nums.length; j++) // as we hold onto that first integer, i, we need to grab another number to compare to i. so we use a for loop to interate through the array, grabbing each remaining number to compare it to i until one of they meets the condition. and if none of them meets our condition, the loop ends, and the first for loop will move on to a new i, starting a new j for loop.... now, we start at j=i+1 because we want to start at the integer after the one we just grabbed - we don't want j to be i or we are just comparing the same number, and as the directions state, we may not use the same element twice. so let j be the number that follows i.
+            if (nums[j] === target - nums[i]) { // if we have i + j = target, and we grabbed i and we are holding onto i until this j-forloop is finished... then we know i, and we know target, so then we could say j = target - i. 
+                return nums = [i,j]
+            }
     }
 }
+
+//With Hashmap:
+
+
+
 //TESTCASES--
 console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
 console.log(twoSum([3, 2, 4], 6), [1, 2]);
