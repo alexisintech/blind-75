7b31ed1bb972def0727b97d6eaa2c13daac7c40b
Alexis Aguilar <alexmerona@gmail.com>
2022-08-11 18:51:00 -0400

    Update product-of-array-except-self.js

--- product-of-array-except-self.js
+++ product-of-array-except-self.js
@@ -37,8 +37,63 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
  * @return {number[]}
  */
 
- var productExceptSelf = function(nums) {
+// Without Map
+
+/* Doesn't work
+var productExceptSelf = function(nums) {
+    let answer = []
+
+    for(let i=0;i<nums.length;i++){
+        let everyNumExceptnumsi = [];
+        for(let j=0;j<nums.length;j++){
+            if (nums[i] !== nums[j]) {
+                everyNumExceptnumsi.push(nums[j])
+            }
+        }
+        answer.push( everyNumExceptnumsi.reduce((a,c) => a*c,1) )
+    }
+
+    return answer
+};
+// DOESN'T WORK. IS GIVING [1,1] AS ANSWER FOR WHEN nums=[0,0] BECAUSE OF THE REDUCE FUNCTION ^^
+*/
+
+
+var productExceptSelf = function(nums) {
+    // Set up an empty array as our result
+    const result = []
     
+    // Initialize a prefix tracker at 1
+    let prefix = 1
+    
+    // Loop through the input array - for each position,
+    // the result array should equal the prefix tracker.
+    
+    // Then, update the prefix tracker to be the product of itself,
+    // multiplied by the input value at the position.
+    for (let i=0; i<nums.length; i++) {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+    
+    // Initialize a suffix tracker at 1
+    let suffix = 1
+    
+    // Loop backwards through the array.
+    // For each iteration, set the result array to be 
+    // the product of itself multiplied by the suffix tracker.
+    
+    // Then, update the suffix tracker to be the product of itself,
+    // multiplied by the input value at that position.
+    for (let i=nums.length - 1; i>=0; i--) {
+        result[i] *= suffix
+        suffix *= nums[i]
+    }
+
+    return result
 };
 
 //TESTCASES--
+console.log(productExceptSelf([1,2,3,4]), [24,12,8,6]);
+console.log(productExceptSelf([-1,1,0,-3,3]), [0,0,9,0,0]);
+console.log(productExceptSelf([0,0]), [0,0]);
