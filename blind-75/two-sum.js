5dd66d500d42b4e29277fc48b546fdb6161df0ec
Alexis Aguilar <alexmerona@gmail.com>
2023-03-08 15:08:45 -0500

    two-sum

--- blind-75/two-sum.js
+++ blind-75/two-sum.js
@@ -84,14 +84,13 @@ const twoSum = (nums, target) => {
 
 const twoSum = (nums, target) => {
     const map = new Map();
+    // Map() { value => index }
 
     for(let i=0;i<nums.length;i++){
         if ( map.has(target - nums[i]) ) {
             return [map.get(target - nums[i]), i];
         } else ( map.set(nums[i], i) )
     }
-        // Map() { value => index }
-    return [];
 }
 
 
